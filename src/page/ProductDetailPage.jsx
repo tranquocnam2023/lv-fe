bd1ed2e6b08ba756a99f650b87f881c6dc593597
tranquocnam2023 <namquoctran2003@gmail.com>
2026-08-13 00:30:41 +0700

    clean: remove unused imports in CartPage, HomePage, and ProductDetailPage

--- src/page/ProductDetailPage.jsx
+++ src/page/ProductDetailPage.jsx
@@ -8,7 +8,7 @@ import { productService } from '../services/productService';
 import { reviewService } from '../services/reviewService';
 import { categoryService } from '../services/categoryService';
 import api from '../services/api';
-import { GitCompare, ChevronLeft, ChevronRight, X, Maximize2, Check, ExternalLink } from 'lucide-react';
+import { Check } from 'lucide-react';
 
 // Subcomponents
 import ProductGallery from './product-detail/components/ProductGallery';
