bfb1ba097667992f396205a54a2d08a58af0cc63
tranquocnam2023 <namquoctran2003@gmail.com>
2026-08-13 00:26:26 +0700

    clean: remove unused icon imports in TransactionModal and AdminDashboard, prop in HistoryTable, and remove deprecated AdminComboModal

--- src/page/admin/combos/AdminComboModal.jsx
+++
@@ -1,6 +0,0 @@
-// Deprecated: AdminComboModal đã được chuyển đổi sang trang Form riêng biệt AdminComboForm.jsx
-import React from 'react';
-
-export default function AdminComboModal() {
-  return null;
-}
